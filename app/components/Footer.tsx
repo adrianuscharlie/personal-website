0901c5e8b43cbcf6041a6ed8246088f64243a899
Charlie <adrianus.charlie02@gmail.com>
2025-04-09 23:22:40 +0700

    fix bug experience card

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
-    <footer className="dark:bg-black dark:text-white">
+    <footer className="bg-black text-white">
       {/* Horizontal line */}
-      <hr className="border-t text-black dark:text-white border-gray-300 dark:border-white py-1" />
-      <p className="text-center text-sm dark:text-white">
+      <hr className="border-t text-white border-white py-1" />
+      <p className="text-center text-smtext-white">
         © {new Date().getFullYear()} Adrianus Charlie H.A. All rights reserved.
       </p>
     </footer>
