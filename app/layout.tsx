9448c3c5a2fe605d83be82e2cfa7927c667427f3
Charlie <adrianus.charlie02@gmail.com>
2025-05-04 11:57:18 +0700

    adding vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
 import Footer from "./components/Footer";
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
+        <Analytics />
         <Navbar />
         {children}
         <Footer />
